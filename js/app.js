3037d2b5e4a5fb8c367df064be3cc6e9da6e4eba
Gabriel Caiana Guedes <gabrielcaianaguedes@gmail.com>
2018-12-04 21:26:10 -0200

    criando variaveis globais

---
+++ js/app.js
@@ -0,0 +1,8 @@
+let selecionado;
+let selecionado_depois;
+let pares = false;
+let list_selecionados = [];
+let list_card;
+let list_encontrados = [];
+let nameClassFirst;
+let nameClassFirstSecond;

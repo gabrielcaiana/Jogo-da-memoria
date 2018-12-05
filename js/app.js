8d299e8c1430012edcef69d72d045bbc9838a824
Gabriel Caiana Guedes <gabrielcaianaguedes@gmail.com>
2018-12-05 00:19:44 -0200

    criando funcao que efetua acoes quando o par esta incorrreto

--- js/app.js
+++ js/app.js
@@ -1,13 +1,14 @@
 let selecionado;
 let selecionado_antes;
-let pares = false;
+let pair = false;
 let list_selecionados = [];
 let list_card;
 let list_encontrados = [];
 let nameClassFirst;
-let nameClassFirstSecond;
+let nameClassSecond;
 let list_card_reais = [];
 let colors = ["#ee82ee","#a9a9a9","#40e0d0","#ffff00" ,"#f0e68c","#dc143c","#7fff00","#f4a460"];
+let index_colors = 0;
 
 controlCard();
 
@@ -33,13 +34,43 @@ function controlCard() {
                 bloquearCard(selecionado,true);
                 list_card_reais[1] = $(this);
                 abrir_ou_fechar(true, selecionado);
+
+                  // Verificando se card 1 é igual ao card 2
+
+                  if(list_selecionados[0] === list_selecionados[1] ){
+                    efeitoCardCorreto(selecionado_antes,false);
+                    if(colors[index_colors]!= null){
+                        igual(colors[index_colors]);
+                        index_colors++;
+                        bloquearCard(list_card_reais[0], true);
+                        bloquearCard(list_card_reais[1], true);
+                        list_card_reais = [];
+                    }   
+
+                }else{
+                    list_card_reais = [];
+                    removeEfeito();
+                    setTimeout(function(){ bloquearCard($('ul.deck li'),true);}, 100);
+                         if(selecionado_antes != null){
+                                 setTimeout(function(){
+                                     abrir_ou_fechar(false,selecionado_antes);
+                                 },1000);
+                             }
+                    bloquearCard(selecionado_antes,false);
+                    naoEIgual();
+                }
             }
 
-            // Verificando se card 1 é igual ao card 2
-
-                if(list_selecionados[0] === list_selecionados[1] ){
-
+            if(list_selecionados.length == 2) {
+                if(pair) {
+                    list_selecionados.splice(0,2);
+                }else {
+                    list_selecionados.splice(0,2);
                 }
+            }
+
+            nameClassSecond = $(this).children().attr('class');
+
         });
     });
 }
@@ -85,8 +116,49 @@ function efeitoCardCorreto(cartaSelecionada, auxiliar) {
             cartaSelecionada.animate({height:"125px"},300);
         }else{
             cartaSelecionada.animate({height:"80px"},300);
-            cartaSelecionada.animate({height:"80px"},450);
+            cartaSelecionada.animate({height:"125px"},450);
         }
     }
 }
+
+//função para adicionar objeto encontrado
+function addClassParaListaEncontrados(objetoEncontrado) {
+    list_encontrados.push(objetoEncontrado);
+}
+
+// função cards iguais colors
+function igual(color) {
+    if(selecionado != null) {
+        
+        setTimeout(function(){
+            if(selecionado != null) {
+                selecionado.css("background-color", color);
+                selecionado_antes.css("background-color", color);
+            }
+        },300);
+
+        setTimeout(function(){
+            efeitoCardCorreto(selecionado,true);
+        },200);
+
+        addClassParaListaEncontrados(selecionado.children().attr('class'));
+        pair = true;
+        list_encontrados.splice(0,2);
+        if(list_encontrados.length == 8) {
+            alert('Você Ganhou :D');
+        }
+
+    }
+} 
+
+// função que efetua ações quando o par esta incorreto
+function naoEIgual() {
+    pair = false;
+    list_selecionados.splice(0,2);
+    setTimeout(function(){
+        bloquearCard(selecionado,false);
+    },1000);
+}
+
+
 
